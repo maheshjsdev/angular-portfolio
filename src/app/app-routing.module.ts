389dbeeaeef57abf503d539b10ce811b529cb74d
maheshjsdev <iammahesh062@gmail.com>
2025-02-04 11:25:45 +0530

    update title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:'', redirectTo:'/profile', pathMatch:'full'},
-  {path:'profile', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
-  {path:'project', loadChildren: () => import('./modules/project/project.module').then(m => m.ProjectModule)},
-  {path:'skill', loadChildren: () => import('./modules/skills/skills.module').then(m => m.SkillsModule)},
+  {path:'profile', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), title: 'MaheshMD | Profile'},
+  {path:'project', loadChildren: () => import('./modules/project/project.module').then(m => m.ProjectModule), title: 'MaheshMD | Project'},
+  {path:'skill', loadChildren: () => import('./modules/skills/skills.module').then(m => m.SkillsModule), title: 'MaheshMD | Skill'},
   // {path:'resume', loadChildren: () => import('./modules/resume/resume.module').then(m => m.ResumeModule)},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
