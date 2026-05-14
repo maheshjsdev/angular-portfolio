306e57eb13ea509367ac1d217d7c3b39bc83f32a
maheshjsdev <iammahesh062@gmail.com>
2026-05-14 18:05:24 +0530

    update project details and reorder entries in project servicet

--- src/app/modules/project/project.service.ts
+++ src/app/modules/project/project.service.ts
@@ -7,15 +7,16 @@ export class ProjectService {
   tableData: any = [
     {
       id: 1,
+      proName: 'BeeduConnect',
+      build: 'HTML, SCSS, Bootstarp, Angular 18, Typescript, .Net, MySQL',
+      path: 'https://edux.jotetx.com/#/vdu/login',
+    },
+
+    {
+      id: 2,
       proName: 'ComeOnTime',
       build: 'HTML, Tailwind CSS, ReactJS, NodeJs, MongoDB',
       path: 'https://comeontime.com',
-    },
-    {
-      id: 2,
-      proName: 'BeeduConnect',
-      build: 'HTML, SCSS, Bootstarp, Angular 18, Typescript, .Net, MySQL',
-      path: 'https://demo.acadamin.com',
     },
     {
       id: 3,
@@ -25,99 +26,100 @@ export class ProjectService {
     },
     {
       id: 4,
+      proName: 'ApiRunner',
+      build: 'HTML, Tailwind CSS, NextJS',
+      path: 'https://apirunner.netlify.app',
+    },
+
+    {
+      id: 5,
       proName: 'Parse2Sheet',
       build: 'HTML, Tailwind CSS, NextJS, AI',
       path: 'https://parse2sheet.web.app',
     },
     {
-      id: 5,
+      id: 6,
       proName: 'Brown Living',
       build: 'HTML, CSS, Bootstrap, Angular 18, Typescript, NodeJs, MySQL',
       path: 'https://app.brownlivingindia.com',
     },
     {
-      id: 6,
+      id: 7,
       proName: 'Mesh Fullstack App',
       build: 'HTML, CSS, Bootstrap, Angular 19, Typescript, NodeJs, MsSQL',
       path: 'https://mesh-devop.web.app',
     },
     {
-      id: 7,
+      id: 8,
       proName: 'Solo',
       build: 'HTML, CSS, Bootstrap, Javascript, Jquery',
       path: './assets/projects/solo/index.html',
     },
     {
-      id: 8,
+      id: 9,
       proName: 'CrudApp',
       build: 'HTML, CSS, Bootstrap, AngularJS',
       path: './assets/projects/crudapp/index.html',
     },
     {
-      id: 9,
+      id: 10,
       proName: 'Organic',
       build: 'HTML, CSS, Bootstrap, Javascript, Jquery',
       path: './assets/projects/organic/index.html',
     },
     {
-      id: 10,
+      id: 11,
       proName: '3lLogics',
       build: 'HTML, CSS, Bootstrap, Javascript, Jquery',
       path: './assets/projects/3lLogics/index.html',
     },
     {
-      id: 11,
+      id: 12,
       proName: 'Zenith',
       build: 'HTML, CSS, Bootstrap, Javascript, Jquery',
       path: './assets/projects/zenith/index.html',
     },
     {
-      id: 12,
+      id: 13,
       proName: 'Zenith Form',
       build: 'HTML, CSS, Bootstrap, AngularJS',
       path: './assets/projects/zenithform/index.html',
     },
     {
-      id: 13,
+      id: 14,
       proName: 'Currency Converter',
       build: 'HTML, CSS, Bootstrap, Javascript',
       path: './assets/projects/currencyConverter/index.html',
     },
     {
-      id: 14,
+      id: 15,
       proName: 'Filter Table Data',
       build: 'HTML, CSS, Bootstrap, Javascript',
       path: './assets/projects/filterTableData/index.html',
     },
     {
-      id: 15,
+      id: 16,
       proName: 'Full Page Scroll',
       build: 'HTML, CSS, Jquery',
       path: './assets/projects/fullPageScroll/index.html',
     },
     {
-      id: 16,
+      id: 17,
       proName: 'Calculator',
       build: 'HTML, CSS, Javascript',
       path: './assets/projects/calculator/index.html',
     },
     {
-      id: 17,
+      id: 18,
       proName: 'Adding Box',
       build: 'HTML, CSS, Bootstrap, AngularJS',
       path: './assets/projects/addingBox/index.html',
     },
     {
-      id: 18,
+      id: 19,
       proName: 'Interest Calculator',
       build: 'HTML, CSS, AgnularJS',
       path: './assets/projects/interestCalculator/index.html',
-    },
-    {
-      id: 19,
-      proName: 'Create Placeholder',
-      build: 'HTML, CSS, Javascript',
-      path: './assets/projects/createplaceholder/index.html',
     },
     {
       id: 20,
@@ -132,7 +134,7 @@ export class ProjectService {
       path: './assets/projects/oopsCrud/crud.html',
     },
   ];
-  constructor() {}
+  constructor() { }
   getAllProject = () => {
     return this.tableData;
   };
